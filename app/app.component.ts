f121e4e43db15cb9e88ee6883a1a174d3256a0e8
Bri <bpopson@gmail.com>
2016-03-25 09:12:26 -0700

    set up hard coded meal examples in app.component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from 'angular2/core';
+import { Meal } from './meal.model';
 
 @Component({
   selector: 'my-app',
@@ -10,4 +11,18 @@ import { Component } from 'angular2/core';
 })
 
 export class AppComponent {
+  public meals: Meal[];
+  constructor(){
+    this.meals = [
+      new Meal("Greek Yogurt", "No sugar added plain", 150, "Breakfast"),
+      new Meal("Coffee", "16oz black", 2, "Drink"),
+      new Meal("String Cheese", "Ate two", 160, "Snack"),
+      new Meal("Club Sandwich", "No mayo", 508, "Lunch"),
+      new Meal("Side Salad", "with vinegarette dressing", 90, "Lunch"),
+      new Meal("Apple", "Golden delicious, medium", 82, "Snack"),
+      new Meal("Shrimp Carbonara", "ate half", 780, "Dinner"),
+      new Meal("Breadsticks", "had three", 420, "Dinner"),
+      new Meal("Red wine", "1 glass", 122, "Drink")
+    ];
+  }
 }
--- app/meal.model.ts
+++ app/meal.model.ts
@@ -1,7 +1,7 @@
 export class Meal {
   constructor(public mealName: string,
     public mealDetails: string,
-    public calorieCount: string,
+    public calorieCount: number,
     public mealTime: string) {
     }
 }
